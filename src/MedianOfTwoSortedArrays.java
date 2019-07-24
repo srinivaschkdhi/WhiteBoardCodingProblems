6cda2485b919603ab912facf60fdd625c5c0bc5e
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-07-24 15:51:14 +0530

    Median of Two Sorted Arrays

--- src/MedianOfTwoSortedArrays.java
+++ src/MedianOfTwoSortedArrays.java
@@ -45,4 +45,52 @@ public class MedianOfTwoSortedArrays {
         return merArr;
     }
 
+    public double findMedianSortedArraysEff(int[] input1, int[] input2) {
+
+        //if input1 is greater than input2 switch them so input1 is smaller
+        if (input1.length > input2.length) findMedianSortedArrays(input2, input1);
+
+        int x = input1.length;
+        int y = input2.length;
+
+        int low = 0;
+        int high = x;
+
+        while (low <= high) {
+
+            int partitionX = (low + high)/2;
+            int partitionY = (x + y + 1)/2 - partitionX;
+
+            //if partitionX == 0 it means no elemetns left on left side Use -INF for maxLeft
+            //if partitionX == y it means no elements left on right side USe +inf for maxright
+
+            int maxLeftX = partitionX == 0 ? Integer.MIN_VALUE : input1[partitionX-1];
+            int minRightX = partitionX == x ? Integer.MAX_VALUE : input2[partitionX];
+
+
+            int maxLeftY = partitionY == 0 ? Integer.MIN_VALUE : input2[partitionY -1];
+            int minRightY = partitionY == y ? Integer.MAX_VALUE : input2[partitionY];
+
+
+            if(maxLeftX <= minRightY && maxLeftY <= minRightX){
+                //We have partioned the array at correct place
+                if(x + y % 2 ==0 ){
+                    return (double)(Math.max(maxLeftX, maxLeftY) + Math.max(minRightX, minRightY)) / 2;
+                }else{
+                    return Math.max(maxLeftX,maxLeftY);
+                }
+
+            }else if(maxLeftX > minRightY){
+                high = partitionX - 1;
+            }else{
+                low = partitionX + 1;
+            }
+
+            throw new IllegalArgumentException();
+        }
+
+
+        return 0;
+    }
+
 }
