df1d3f4002550634bde91c7824f5037b12c955e2
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-09-09 20:48:22 +0530

    Longest Common Prefix in list of strings

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+Example 1:
+Input: ["flower","flow","flight"]
+Output: "fl"
+ */
+
+public class LongestCommonPrefix{
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{"srinivas","srini0","srinu"}));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+    }
+}
