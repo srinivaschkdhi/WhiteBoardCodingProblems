d932a9855fef0e6ee1298f46d837a4983e411a64
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-11 20:30:12 +0530

    Create NumberOfIslands.java

---
+++ src/NumberOfIslands.java
@@ -0,0 +1,42 @@
+public final class NumberOfIslands {
+
+    public static int numIslands(char[][] grid) {
+        int count = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    count = count + 1;
+                    callBFS(grid, i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void callBFS(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+        callBFS(grid, i + 1, j); //up
+        callBFS(grid, i - 1, j);//down
+        callBFS(grid, i, j - 1);//left
+        callBFS(grid, i, j + 1);//right
+
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'},
+        };
+
+        int numOfIslands = numIslands(grid);
+        System.out.println("numOfIslands = " + numOfIslands);
+    }
+}
