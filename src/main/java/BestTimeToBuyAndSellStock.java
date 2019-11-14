d980d3170d22573315793586773c59c18120bcf7
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-14 22:42:38 +0530

    Create BestTimeToBuyAndSellStock.java

---
+++ src/main/java/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,19 @@
+
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+        int minVal = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minVal) {
+                minVal = prices[i];
+            } else if (prices[i] - minVal > maxProfit) {
+                maxProfit = prices[i] - minVal;
+            }
+        }
+
+        return maxProfit;
+
+    }
+}
