9a51c0f166d99ede01aa121c5049ba98d8199292
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-17 01:47:16 +0530

    Create LongPresedName.java

---
+++ src/main/java/LongPresedName.java
@@ -0,0 +1,25 @@
+public class LongPresedName {
+    public boolean isLongPressedName(String name, String typed) {
+        if (name.length() == typed.length()) {
+            return true;
+        }
+        if (name.length() > typed.length()) {
+            return false;
+        }
+
+        int i = 0;
+        int j = 0;
+
+        while (j < typed.length()) {
+
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+                i++;
+            } else if (j == 0 || typed.charAt(j) != typed.charAt(j - 1)) {
+                return false;
+            }
+            j++;
+        }
+
+        return i == name.length();
+    }
+}
