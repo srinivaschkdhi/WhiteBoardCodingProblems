cc6309147329798c7de9042c308e20bea7937018
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-16 12:30:56 +0530

    Create LongestSubstringWithoutRepeatingCharacters.java

---
+++ src/main/java/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        int aPtr = 0;
+        int bPtr = 0;
+        int max = 0;
+
+        HashSet<Character> hashSet = new HashSet<>();
+
+        while (bPtr < s.length()) {
+            if (!hashSet.contains(s.charAt(bPtr))) {
+                hashSet.add(s.charAt(bPtr));
+                bPtr++;
+                max = Math.max(max, hashSet.size());//bPtr - aPtr
+            } else {
+                hashSet.remove(s.charAt(aPtr));
+                aPtr++;
+            }
+        }
+
+        return max;
+
+    }
+}
