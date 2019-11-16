e28e480fda80c8fc4e46fdadcfbf728bcb275125
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-16 11:53:33 +0530

    Create FruitsIntoBaskets.java

---
+++ src/main/java/FruitsIntoBaskets.java
@@ -0,0 +1,35 @@
+
+public class FruitsIntoBaskets {
+    public int totalFruit(int[] tree) {
+        int lastFruit = -1;
+        int secondLastFruit = -1;
+
+        int lastFruitCount = 0;
+        int currMax = 0;
+        int max = 0;
+
+        for (int fruit : tree) {
+
+            if (fruit == lastFruit || fruit == secondLastFruit) {
+                currMax += 1;
+            } else {
+                currMax = lastFruitCount + 1;
+            }
+
+            if (fruit == lastFruit) {
+                lastFruitCount += 1;
+            } else {
+                lastFruitCount = 1;
+            }
+
+            if (fruit != lastFruit) {
+                secondLastFruit = lastFruit;
+                lastFruit = fruit;
+            }
+
+            max = Math.max(max, currMax);
+        }
+
+        return max;
+    }
+}
