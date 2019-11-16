74c964ebe24ed2e06119d4232927c35a6ad20939
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-17 01:30:55 +0530

    Create BuddyStrings.java

---
+++ src/main/java/BuddyStrings.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BuddyStrings {
+    public boolean buddyStrings(String A, String B) {
+
+        if (A.length() != B.length()) {
+            return false;
+        }
+
+        if (A.equals(B)) {
+            Set<Character> uniqChars = new HashSet<>();
+            for (char c : A.toCharArray()) {
+                uniqChars.add(c);
+            }
+
+            if (uniqChars.size() < A.length()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        List<Integer> diffCharIndexes = new ArrayList<>();
+        for (int i = 0; i < A.length(); i++) {
+            if (A.charAt(i) != B.charAt(i)) {
+                diffCharIndexes.add(i);
+            }
+        }
+        if (diffCharIndexes.size() == 2 && A.charAt(diffCharIndexes.get(0)) == B.charAt(diffCharIndexes.get(1)) &&
+                B.charAt(diffCharIndexes.get(0)) == A.charAt(diffCharIndexes.get(1))) {
+            return true;
+
+        } else {
+            return false;
+        }
+
+    }
+}
