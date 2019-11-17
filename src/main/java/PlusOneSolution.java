bc6fdb076526415238c945c1186fe7c0c56f1d1e
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-17 20:23:27 +0530

    Create PlusOneSolution.java

---
+++ src/main/java/PlusOneSolution.java
@@ -0,0 +1,18 @@
+
+public class PlusOneSolution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] <= 8) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int[n + 1];//by default all other values are 0.only need to set first digit
+        newNumber[0] = 1;
+        return newNumber;
+    }
+}
