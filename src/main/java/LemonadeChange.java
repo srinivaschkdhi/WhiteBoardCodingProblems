901b2dcaf9a52a3ee90b92e4a41b713acfe16da3
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-18 22:37:58 +0530

    Create LemonadeChange.java

---
+++ src/main/java/LemonadeChange.java
@@ -0,0 +1,26 @@
+public class LemonadeChange {
+    public boolean lemonadeChange(int[] bills) {
+        int fives = 0;
+        int tens = 0;
+        
+        for(int bill : bills){
+            if(bill == 5)
+                fives ++;
+            else if(bill == 10){
+                tens++;
+                fives--;
+            }else if(bill == 20 && tens >0){
+                tens--;
+                fives--;
+            }
+            else if(bill == 20 && tens >=0)
+                fives= fives - 3;
+            
+            if(fives < 0)
+                return false;
+                
+        }
+        
+        return true;
+    }
+}
