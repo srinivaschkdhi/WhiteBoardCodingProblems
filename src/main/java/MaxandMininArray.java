c7e72f0895f4aadc5c78fca60d503166b471c0bf
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-21 18:33:58 +0530

    Update MaxandMininArray.java

--- src/main/java/MaxandMininArray.java
+++ src/main/java/MaxandMininArray.java
@@ -10,7 +10,7 @@ public class MaxandMininArray {
         int n = A.length;
 
 
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n - 1; i += 2) {
             if (A[i] < A[i + 1]) {
                 if (A[i] < small)
                     small = A[i];
