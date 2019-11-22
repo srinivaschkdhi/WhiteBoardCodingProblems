6db7b1ff96ca24bfc85714208312d9211f622b37
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-22 23:57:17 +0530

    Update RemoveVowels.java

--- src/main/java/RemoveVowels.java
+++ src/main/java/RemoveVowels.java
@@ -3,4 +3,24 @@ public class RemoveVowels {
     public String removeVowels(String S){
         return S.replaceAll("[aeiou]","");
     }
+    
+    public String removeVowels2(String S){
+        Set<Character> vowels = new HashSet<Character>(){{
+            add('a');
+            add('e');
+            add('i');
+            add('o');
+            add('u');
+        }};
+        
+        StringBuilder result = new StringBuilder();
+        for(char c:S.toCharArray()){
+            if(!vowels.contains(c)){
+                result.append(c);
+            }
+        }
+        
+       return result;
+      
+    }
 }
