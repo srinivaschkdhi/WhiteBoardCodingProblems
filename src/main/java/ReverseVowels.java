cd190beaf9b961912168d18b74abe1f40ee5fb42
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-22 22:24:22 +0530

    Create ReverseVowels.java

---
+++ src/main/java/ReverseVowels.java
@@ -0,0 +1,40 @@
+class ReverseVowels {
+    public String reverseVowels(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        HashSet<Character> vowels = new HashSet<Character>(){{
+            add('a');
+            add('e');
+            add('i');
+            add('o');
+            add('u');
+            add('A');
+            add('E');
+            add('I');
+            add('O');
+            add('U');
+        }};
+        
+        char[] A = s.toCharArray();
+        
+        while(left < right){
+
+            while(left < right && !vowels.contains(A[left]))
+                left++;
+            
+            while(left < right && !vowels.contains(A[right]))
+                right--;
+            
+            if(left < right){
+                char temp = A[left];
+                A[left] = A[right];
+                A[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        
+        return new String(A);    
+        }
+}
