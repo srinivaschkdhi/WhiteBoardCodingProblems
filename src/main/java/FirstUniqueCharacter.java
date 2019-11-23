2ea089ed547ffc936408afcf7e16d8048f33d208
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-23 13:56:22 +0530

    Create FirstUniqueCharacter.java

---
+++ src/main/java/FirstUniqueCharacter.java
@@ -0,0 +1,23 @@
+public class FirstUniqueCharacter {
+    public int firstUniqChar(String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        
+        
+        for(int i = 0;i <s.length();i++){
+            char c = s.charAt(i);
+            if(map.containsKey(c)){
+                map.put(c,-1);
+            }else{
+                map.put(c,i);
+            }
+        }
+        
+        int min = Integer.MAX_VALUE;
+        
+        for(char c : map.keySet()){
+            if(map.get(c) != -1 && map.get(c) < min)
+                min = map.get(c);
+        }
+        return (min == Integer.MAX_VALUE)?-1:min;
+    }
+}
