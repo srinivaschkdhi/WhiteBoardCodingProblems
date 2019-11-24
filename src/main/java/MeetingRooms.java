b308e30ab70b6ea98048ad6faa35d06f70f13761
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-24 12:49:18 +0530

    Create MeetingRooms.java

---
+++ src/main/java/MeetingRooms.java
@@ -0,0 +1,23 @@
+public class MeetingRooms {
+  public boolean canAttendMeetings(Interval[] intervals){
+    int[] starts = new int[intervals.length];
+    int[] ends = new int[intervals.length];
+    
+    for(int i = 0; i < intervals.length;i++){
+      starts[i] = intervals[i].start;
+      ends[i] = intervals[i].end;
+      }
+      
+      Arrays.sort(starts);
+      Arrays.sort(ends);
+      
+      
+      for(int i = 0;i < starts.length-1;i++){
+      if(starts[i + 1] < ends[i]){
+      return false;
+      }
+      }
+      
+      return true;
+      }
+      }
