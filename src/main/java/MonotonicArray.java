62c9a2720684e3d94074248fbdd2df6a29fc77a6
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-12-03 23:06:40 +0530

    Create MonotonicArray.java

---
+++ src/main/java/MonotonicArray.java
@@ -0,0 +1,16 @@
+public class MonotonicArray {
+    public boolean isMonotonic(int[] A) {
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
+
+        for (int i = 0; i < A.length - 1; i++) {
+            if (A[i] > A[i + 1])
+                isIncreasing = false;
+
+            if (A[i] < A[i + 1])
+                isDecreasing = false;
+        }
+
+        return isDecreasing || isIncreasing;
+    }
+}
