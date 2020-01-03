55605e912ce058a42679c25147230dfef884b67f
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-01-03 06:38:04 +0530

    repeated substring pattern

---
+++ src/main/java/RepeatedSubstringPattern.java
@@ -0,0 +1,46 @@
+/*
+iven a non-empty string check if it can be constructed by taking a substring of it and appending
+multiple copies of the substring together. You may assume the given string consists of lowercase
+English letters only and its length will not exceed 10000.
+
+ Example 1:
+
+Input: "abab"
+Output: True
+Explanation: It's the substring "ab" twice.
+
+Example 2:
+
+Input: "aba"
+Output: False
+
+Example 3:
+
+Input: "abcabcabcabc"
+Output: True
+Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+
+ */
+public class RepeatedSubstringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        int len = s.length();
+
+        for (int i = len / 2; i >= 1; i--) {
+            if (len % i == 0) {
+
+                int num_repeats = len / i;
+                String substring = s.substring(0, i);
+                StringBuilder stringBuilder = new StringBuilder();
+
+                for (int j = 0; j < num_repeats; j++) {
+                    stringBuilder.append(substring);
+                }
+
+                if (stringBuilder.toString().equals(s))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
