580436b088e2c6c52b19a3ab122c785e07c06cb3
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-02-21 00:07:23 +0530

    Merge K sorted Lists

---
+++ src/main/java/MergeKSortedLists.java
@@ -0,0 +1,30 @@
+import java.util.PriorityQueue;
+
+/**
+ * @author Srinivas_Chintakindhi
+ */
+
+public class MergeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (ListNode head : lists) {
+            while (head != null) {
+                minHeap.add(head.val);
+                head = head.next;
+            }
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+
+        while (!minHeap.isEmpty()) {
+            head.next = new ListNode(minHeap.remove());
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+}
+
+
