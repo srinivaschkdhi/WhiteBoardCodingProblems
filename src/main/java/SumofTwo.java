9d05bb32d17560d107fe6ac59b7ca9357f46f642
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-02-22 00:51:38 +0530

    sum of two

--- src/main/java/SumofTwo.java
+++ src/main/java/SumofTwo.java
@@ -1,6 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Srinivas_Chintakindhi
  */
 
 public class SumofTwo {
+    public boolean sumOfTwo(int[] nums1, int[] nums2, int sum) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums1) {
+            int diff = sum - num;
+            set.add(diff);
+        }
+
+        for (int num : nums2) {
+            if (set.contains(num))
+                return true;
+        }
+
+        return false;
+    }
 }
