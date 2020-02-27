fc2d851f8274d4b246d230934363956f8dfb6efa
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-02-28 00:24:56 +0530

    Reverse of string without using temporay variable

--- src/main/java/ReverseString.java
+++ src/main/java/ReverseString.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ReverseString {
     public void reverseString(char[] s) {
         int i = 0;
@@ -12,5 +14,24 @@ public class ReverseString {
         }
         
         
+    }
+
+    public void reverseString2(char[] s){
+        int i = 0;
+        int j = s.length - 1;
+
+        while(i < j){
+            s[i] ^= s[j];
+            s[j] ^= s[i];
+            s[i] ^= s[j];
+
+            ++i;
+            --j;
+        }
+        System.out.println(Arrays.toString(s));
+    }
+
+    public static void main(String[] args) {
+        new ReverseString().reverseString2("Srinivas".toCharArray());
     }
 }
