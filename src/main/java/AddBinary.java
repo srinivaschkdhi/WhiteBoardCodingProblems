130eebfd431ffb14f3c01fa2aedb3e4db466192d
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-04-29 12:44:37 +0530

    Add Binary Refactor

--- src/main/java/AddBinary.java
+++ src/main/java/AddBinary.java
@@ -5,15 +5,15 @@ public class AddBinary {
         int i = a.length() - 1;
         int j = b.length() - 1;
         int carry = 0;
+        int sum = 0;
 
         while (i >= 0 || j >= 0) {
-            int sum = carry;
-            if (i >= 0) {
-                sum += a.charAt(i) - '0';
-            }
-            if (j >= 0) {
-                sum += b.charAt(j) - '0';
-            }
+
+            int x = (i >= 0) ? a.charAt(i) - '0' : 0;
+            int y = (j >= 0) ? b.charAt(j) - '0' : 0;
+
+            sum = carry + x + y;
+
             sb.append(sum % 2);
 
             carry = sum / 2;
