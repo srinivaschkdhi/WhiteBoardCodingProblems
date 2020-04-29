097d9439e7a192408a5d34aa77d66870140c45e9
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-04-29 16:19:04 +0530

    Added another way to solve all anagrams

--- src/main/java/AllAnagrams.java
+++ src/main/java/AllAnagrams.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -39,8 +40,51 @@ public class AllAnagrams {
         return result;
     }
 
+    public List<Integer> findAnagrams2(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+
+        if(s.length() < p.length())
+            return result;
+
+        int start = 0;
+        int end = p.length() - 1;
+
+        int[] s_freq = getCharsCountStartToEnd(s,end);
+        int[] p_freq = getCharsCountStartToEnd(p,end);
+
+        if(Arrays.equals(p_freq,s_freq))
+            result.add(start);
+
+        start++;
+        end++;
+
+        while(end < s.length()){
+            s_freq[s.charAt(start - 1)-'a']--;
+            s_freq[s.charAt(end)-'a']++;
+
+            if(Arrays.equals(p_freq,s_freq))
+                result.add(start);
+
+            start++;
+            end++;
+
+        }
+        return result;
+    }
+
+    private int[] getCharsCountStartToEnd(String s,int end) {
+        int[] s_freq = new int[26];
+        for (int i = 0; i <= end; i++) {
+            char c = s.charAt(i);
+            s_freq[c - 'a']++;
+        }
+        return s_freq;
+    }
+
     public static void main(String[] args) {
         List<Integer> anagrams = new AllAnagrams().findAnagrams("cbaebabacd", "abc");
+        System.out.println(anagrams);
+        anagrams = new AllAnagrams().findAnagrams2("cbaebabacd", "abc");
         System.out.println(anagrams);
     }
 }
