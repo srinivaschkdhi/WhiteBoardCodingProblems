52489fbccfd43552b440b4737721d9734c8f84e1
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-04-29 18:38:50 +0530

    BestTimeToBuyAndSellStock Refactor

--- src/main/java/BestTimeToBuyAndSellStock.java
+++ src/main/java/BestTimeToBuyAndSellStock.java
@@ -2,18 +2,18 @@
 public class BestTimeToBuyAndSellStock {
 
     public int maxProfit(int[] prices) {
-        int minVal = Integer.MAX_VALUE;
+       int minVal = Integer.MAX_VALUE;
         int maxProfit = 0;
 
         for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < minVal) {
+            if (prices[i] < minVal) {//if it is min value . buy it . we dont sell .
                 minVal = prices[i];
-            } else if (prices[i] - minVal > maxProfit) {
-                maxProfit = prices[i] - minVal;
+            } else {// if it  is not min value . we already might bought for min value. we try to sell for the price .
+                int curProfit = prices[i] - minVal;
+                maxProfit = Math.max(curProfit,maxProfit);
             }
         }
-
+      
         return maxProfit;
-
     }
 }
