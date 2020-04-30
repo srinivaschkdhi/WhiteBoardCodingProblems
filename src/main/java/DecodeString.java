7a20458821d7c7ccf88953a5a6917e1d9f3343c2
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-04-30 11:42:04 +0530

    Refactor DecodeString

--- src/main/java/DecodeString.java
+++ src/main/java/DecodeString.java
@@ -1,35 +1,49 @@
+import java.util.Stack;
+
 public class DecodeString {
     public String decodeString(String s) {
-        Stack<Integer> counts = new Stack<>();
-        Stack<String> result = new Stack<>();
-        String res = "";
+        Stack<Integer> countsStack = new Stack<>();
+        Stack<String> trackCharsStack = new Stack<>();
+
         int index = 0;
+        String trackChars = "";
 
         while (index < s.length()) {
+
+
             if (Character.isDigit(s.charAt(index))) {
+
                 int count = 0;
                 while (Character.isDigit(s.charAt(index))) {
-                    count = count * 10 + (s.charAt(index) - '0');
+                    count = count * 10 + s.charAt(index) - '0';
                     index++;
                 }
-                counts.push(count);
+
+                countsStack.push(count);
+
             } else if (s.charAt(index) == '[') {
-                result.push(res);
-                res = "";
+
+                trackCharsStack.push(trackChars);
+                trackChars = "";
                 index++;
+
             } else if (s.charAt(index) == ']') {
-                StringBuilder sb = new StringBuilder(result.pop());
-                int count = counts.pop();
+
+                StringBuilder sb = new StringBuilder(trackCharsStack.pop());
+                int count = countsStack.pop();
+
                 for (int i = 0; i < count; i++) {
-                    sb.append(res);
+                    sb.append(trackChars);
                 }
-                res = sb.toString();
+
+                trackChars = sb.toString();
                 index++;
+
             } else {
-                res += s.charAt(index);
+                trackChars += s.charAt(index);
                 index++;
             }
         }
-        return res;
+        return trackChars;
     }
 }
