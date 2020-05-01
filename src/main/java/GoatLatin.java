1282e6b652235d811c238a802eb36dcea0b9fb59
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-05-01 14:17:44 +0530

    Refactor GoatLatin

--- src/main/java/GoatLatin.java
+++ src/main/java/GoatLatin.java
@@ -1,34 +1,41 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class GoatLatin {
     public String toGoatLatin(String S) {
-        HashSet<Character> vowels = new HashSet<>();
-        
-        for(char c : "aeiouAEIOU".toCharArray()){
-            vowels.add(c);
-        }
-        
+        Set<Character> vowels = getVowels();
+
         String result = "";
-        int index = 1;
-        
-        for(String word : S.split("\\s")){
-            if(index > 1){
-                result += " ";
-            }
-            
+        int wordPosInSen = 1;
+
+        for(String word : S.split(" ")){
+
+            if(wordPosInSen > 1)
+                result = result + " "; // from 2 word onwards adding space to left of the word.
+
             char firstLetter = word.charAt(0);
+
             if(vowels.contains(firstLetter)){
                 result += word + "ma";
-            }else {
-                result += word.substring(1)+firstLetter+"ma";
+            }else{
+                result += word.substring(1) + firstLetter + "ma";
             }
-            
-        
-            for(int j =0;j<index;j++){
+
+            for(int j = 0; j < wordPosInSen ; j++){//add no of as based on wordpos.
                 result += "a";
             }
-            
-            index++;
+
+            wordPosInSen++;
         }
-        
         return result;
+    }
+
+    private HashSet<Character> getVowels() {
+        HashSet<Character> vowels = new HashSet<>();
+
+        for (char c : "aeiouAEIOU".toCharArray()) {
+            vowels.add(c);
+        }
+        return vowels;
     }
 }
