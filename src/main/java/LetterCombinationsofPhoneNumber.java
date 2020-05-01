7641ffcab684b8752f362487d4c791aef6b0e319
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-05-01 21:08:38 +0530

    Refactor Letter Combinations of PhoneNumber

--- src/main/java/LetterCombinationsofPhoneNumber.java
+++ src/main/java/LetterCombinationsofPhoneNumber.java
@@ -13,55 +13,11 @@ A mapping of digit to letters (just like on the telephone buttons) is given belo
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Queue;
 
 
 public class LetterCombinationsofPhoneNumber {
-    Map<String, String> phone = new HashMap<String, String>() {{
-        put("2", "abc");
-        put("3", "def");
-        put("4", "ghi");
-        put("5", "jkl");
-        put("6", "mno");
-        put("7", "pqrs");
-        put("8", "tuv");
-        put("9", "wxyz");
-    }};
-
-    List<String> output = new ArrayList<String>();
-
-    public void backtrack(String combination, String next_digits) {
-        // if there is no more digits to check
-        if (next_digits.length() == 0) {
-            // the combination is done
-            output.add(combination);
-        }
-        // if there are still digits to check
-        else {
-            // iterate over all letters which map
-            // the next available digit
-            String digit = next_digits.substring(0, 1);
-            String letters = phone.get(digit);
-            for (int i = 0; i < letters.length(); i++) {
-                String letter = phone.get(digit).substring(i, i + 1);
-                // append the current letter to the combination
-                // and proceed to the next digits
-                backtrack(combination + letter, next_digits.substring(1));
-            }
-        }
-    }
-
-    public List<String> letterCombinations(String digits) {
-        if (digits.length() != 0) {
-            backtrack("", digits);
-        }
-        return output;
-    }
-
     public List<String> letterCombinations2(String digits) {
         Queue<String> queue = new ArrayDeque<>();
 
@@ -70,15 +26,17 @@ public class LetterCombinationsofPhoneNumber {
         }
         queue.add("");
 
-        String[] charMap = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        String[] keyPad = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
         for (int i = 0; i < digits.length(); i++) {
-            int index = Character.getNumericValue(digits.charAt(i));
+            int indexInKeyPad = digits.charAt(i) - '0';
 
-            while (queue.peek().length() == i) {
-                String permutation = queue.remove();
-                for (char c : charMap[index].toCharArray()) {
-                    queue.add(permutation + c);
+            while (queue.peek().length() == i) { // loop queue to pull all i size elements
+
+                String iLenStr = queue.remove();
+
+                for (char c : keyPad[indexInKeyPad].toCharArray()) {
+                    queue.add(iLenStr + c);
                 }
             }
         }
@@ -87,13 +45,15 @@ public class LetterCombinationsofPhoneNumber {
     }
 
     public static void main(String[] args) {
-        LetterCombinationsofPhoneNumber letterCombinationsofPhoneNumber = new LetterCombinationsofPhoneNumber();
-        List<String> combinations = letterCombinationsofPhoneNumber.letterCombinations("234");
-        System.out.println("combinations = " + combinations);
-        List<String> strings = letterCombinationsofPhoneNumber.letterCombinations2("234");
+        List<String> strings = new LetterCombinationsofPhoneNumber().letterCombinations2("234");
         System.out.println("strings = " + strings);
-        System.out.println(combinations.equals(strings));
     }
-
-
 }
+//
+
+//"234"
+//indexes = 0,1,2
+// at i = 0, for 0th index input we add all 0 length elements of queue
+// at i = 1, for 1th index input we add all 1 length elements of queue
+// at i = 2, for 2th index input we add all 2 length elements of queue
+// for ith index input add all i size elements of queue
