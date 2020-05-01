d6b4998531faf1ccf5c1bb8ec517a3fed90c346a
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-05-01 12:05:55 +0530

    Refactor Flip Game

--- src/main/java/FlipGame.java
+++ src/main/java/FlipGame.java
@@ -1,16 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FlipGame {
   public List<String> generatePossibleNextMoves(String s){
-      List<String> possibleStates = new ArrayLis<String>();
+      List<String> possibleStates = new ArrayList<String>();
       int i = 0;
+
       while(i < s.length()){
+
         int nextMove = (i == 0)?s.indexOf("++"):s.indexOf("++",i);
+        
         if(nextMove == -1){
           return possibleStates;
          }
          
-         String nexState = s.substring(0,nextMove)+"--"+s.substring(nextMove+2);
+         String nextState = s.substring(0,nextMove)+"--"+s.substring(nextMove+2);
          possibleStates.add(nextState);
-         i = nextState + 1;
+         i = nextMove + 1;
      }
      
      return possibleStates;
