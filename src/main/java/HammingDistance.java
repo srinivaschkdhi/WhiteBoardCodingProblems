d55b1b8f17c948bbea58dec7f66adb8a5b148b03
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-05-01 15:14:47 +0530

    Refactor Hamming Distance

--- src/main/java/HammingDistance.java
+++ src/main/java/HammingDistance.java
@@ -1,13 +1,34 @@
 public class HammingDistance {
     public int hammingDistance(int x, int y) {
-        int result = 0;
+        int numOfDiff = 0;
         
         while(x > 0 || y > 0){
-            result += (x % 2) ^ (y % 2);
+            numOfDiff += (x % 2) ^ (y % 2);
+            
             x >>= 1;
             y >>= 1;
         }
         
-        return result;
+        return numOfDiff;
+    }
+
+    public int hammingDistance2(int x, int y) {
+        int numOfDiffPos = 0;
+        
+        while(x > 0 || y > 0){
+            int bit1 = x % 2;
+            int bit2 = y % 2 ;
+            
+            int xorResl = bit1 ^ bit2;
+            
+            if(xorResl == 1){
+                numOfDiffPos += 1;   
+            }
+            
+            x >>= 1;
+            y >>= 1;
+        }
+        
+        return numOfDiffPos;
     }
 }
