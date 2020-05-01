41b853a3afc4877995f03d3cf173433d2828fb86
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-05-01 16:16:45 +0530

    Refactor Intersection of Two Arrays

--- src/main/java/IntersectionofTwoArrays.java
+++ src/main/java/IntersectionofTwoArrays.java
@@ -2,26 +2,30 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class IntersectionofTwoArrays  {
-    public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set = new HashSet<>();
-        
-        for(int i : nums1){
-            set.add(i);
+     public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> firstArrayElems = new HashSet<>();
+
+        for (int i : nums1) {
+            firstArrayElems.add(i);
         }
-        
+
         Set<Integer> intersection = new HashSet<>();
-        for(int i: nums2){
-            if(set.contains(i))
+        for (int i : nums2) {
+            if (firstArrayElems.contains(i))
                 intersection.add(i);
         }
-        
+
+        int[] result = convertToArray(intersection);
+        return result;
+    }
+
+    private int[] convertToArray(Set<Integer> intersection) {
         int[] result = new int[intersection.size()];
         int index = 0;
-        for(int i:intersection){
+        for (int i : intersection) {
             result[index] = i;
             index++;
         }
-        
         return result;
     }
 }
