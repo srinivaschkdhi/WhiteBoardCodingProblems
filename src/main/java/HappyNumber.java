9f43da4c3e80b22cb02d21b3a51d2ed6cb14f92e
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-05-01 15:33:55 +0530

    Refactor Happy Number

--- src/main/java/HappyNumber.java
+++ src/main/java/HappyNumber.java
@@ -4,22 +4,22 @@ import java.util.Set;
 public class HappyNumber {
     public boolean isHappy(int n) {
         Set<Integer> seen = new HashSet<>();
-
-        while (n != 1) {
-            int curr = n;
+        
+        while(n != 1){
             int sum = 0;
-
-            while (curr != 0) {
-                sum += (curr % 10) * (curr % 10);
-                curr /= 10;
+            
+            while(n != 0){
+                sum += (n % 10) * (n  %  10);
+                n /= 10;
             }
-
-            if (seen.contains(sum))
+            
+            if(seen.contains(sum))
                 return false;
-
             seen.add(sum);
+            
             n = sum;
         }
+        
         return true;
     }
 }
