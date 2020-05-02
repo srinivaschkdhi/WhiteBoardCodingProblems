4bf648c96a5ad1aa90b09ad624ca80dd7332b23c
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-05-02 19:04:36 +0530

    UniqueNumbersLenInDuplicatedSortedArray.java

---
+++ src/main/java/UniqueNumbersLenInDuplicatedSortedArray.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+/**
+ * @author Srinivas_Chintakindhi
+ */
+
+public class UniqueNumbersLenInDuplicatedSortedArray {
+    public static int getUniqueNumberLengthWithRemovingDuplicates(int[] arr) {
+        if (arr.length <= 1)
+            return arr.length;
+
+        int start = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1])
+                arr[start++] = arr[i];
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        return start;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {1, 4, 4, 4, 5, 6, 7, 7, 8};
+
+        System.out.println(new UniqueNumbersLenInDuplicatedSortedArray().
+                getUniqueNumberLengthWithRemovingDuplicates(arr));
+
+    }
+}
