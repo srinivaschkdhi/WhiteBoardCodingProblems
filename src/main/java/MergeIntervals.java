9d9b1104e151157bb001ba6296c80a84d278002f
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-05-02 15:18:29 +0530

    Refactor Intervals

--- src/main/java/MergeIntervals.java
+++ src/main/java/MergeIntervals.java
@@ -4,29 +4,36 @@ import java.util.List;
 
 public class MergeIntervals {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length <= 1) {
+        if(intervals.length <= 1)
             return intervals;
-        }
-        Arrays.sort(intervals, (arr1, arr2) -> Integer.compare(arr1[0], arr2[0]));
+        
+        
+        Arrays.sort(intervals,(arr1,arr2)->  Integer.compare(arr1[0],arr2[0]));
         List<int[]> result = new ArrayList<>();
-
+            
+        
         int[] currInterval = intervals[0];
         result.add(currInterval);
-
-        for (int[] interval : intervals) {
-            int currBegin = currInterval[0];
-            int currEnd = currInterval[1];
+        
+        
+        
+        for(int i = 1; i < intervals.length;i++){
+            int[] interval = intervals[i];
+            
+            int curBegin = currInterval[0];
+            int curEnd = currInterval[1];
+            
             int nextBegin = interval[0];
             int nextEnd = interval[1];
-
-            if (currEnd >= nextBegin) {
-                currInterval[1] = Math.max(nextEnd, currEnd);
-            } else {
+            
+            if(curEnd < nextBegin){
+                result.add(interval);
                 currInterval = interval;
-                result.add(currInterval);
+            }else{
+                currInterval[1] = Math.max(nextEnd,curEnd);
             }
         }
-
+        
         return result.toArray(new int[result.size()][]);
     }
     
