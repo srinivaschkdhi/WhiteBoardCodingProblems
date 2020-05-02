6f27f285f1a38b511c96185886ec580af52c466c
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-05-02 16:39:06 +0530

    Refactor Min in Rotated sorted array

--- src/main/java/MinInRotatedSortedArray.java
+++ src/main/java/MinInRotatedSortedArray.java
@@ -1,29 +1,33 @@
 
-public class MinInRotatedSortedArray {
+public class MinInRotatedSortedA {
     public int findMin(int[] A) {
-        if (A.length == 0) {
-            return -1;
-        }
-        if (A.length == 1) {
-            return A[0];
+         if (A == null || A.length == 0)
+                return 0;
+
+        // Case when A is not rotated. Then first index is the pivot
+        if (A.length == 1 || A[0] < A[A.length - 1]) {
+            return 0;
         }
 
         int low = 0;
         int high = A.length - 1;
 
         while (low <= high) {
-            int mid = low + (high - low) / 2;
+            int mid = (low + high) / 2;
 
-            if (mid > 0 && A[mid] < A[mid - 1]) {
-                return A[mid];
-            } else if (A[low] <= A[mid] && A[high] < A[mid]) {//left is sorted eliminate left part
+            if (mid < A.length - 1 && A[mid] > A[mid + 1])
+                return A[mid + 1];
+            else if (A[low] <= A[mid]) {
+                // If A[start] <= A[mid],
+                // it means from start to mid, all elements are in sorted order,
+                // so pivot will be in second half
                 low = mid + 1;
-            } else {
+            } else
+                // else pivot lies in first half, so we find the pivot in first half
                 high = mid - 1;
-            }
         }
 
-        return A[low];
+        return 0;
     }
 
 }
