529f941414b51379b7e57c91c6dbe9cce1fadbec
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2021-07-06 14:15:34 +0530

    Update SingleNumberII.java

--- src/main/java/SingleNumberII.java
+++ src/main/java/SingleNumberII.java
@@ -20,6 +20,33 @@ class SingleNumberII {
         return result;
     }
 
+    public int singleNumber2(int[] nums) {
+        Arrays.sort(nums);
+        
+        int n = nums.length;
+        
+        
+        if(n < 3)
+            return nums[0];
+        
+        
+        if(nums[0] != nums[1])
+            return nums[0];
+        
+        if(nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
+     
+        
+        for(int i = 1; i <n; i=i+3){
+                
+            if(nums[i] != nums[i - 1])
+                return nums[i - 1];
+            
+        }
+        
+        return -1;
+        
+    }
     public static void main(String[] args) {
         new SingleNumberII().singleNumber(new int[]{2,3,2,2});
     }
