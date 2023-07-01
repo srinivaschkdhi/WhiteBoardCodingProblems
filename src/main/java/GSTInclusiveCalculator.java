da3fca6cc0e8c7852aeb89471c0f06a77bcb2e0c
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2023-07-01 22:51:40 +0530

    Update GSTInclusiveCalculator.java

--- src/main/java/GSTInclusiveCalculator.java
+++ src/main/java/GSTInclusiveCalculator.java
@@ -1,23 +1,23 @@
-import java.text.DecimalFormat;
+import java.text.Format;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class GSTInclusiveCalculator {
     private static final int HOURLY_RATE = 1700;
     private static final int GST_PERCENTAGE = 18;
     private static final int WORK_HOURS_PER_DAY = 8;
-    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#0.00");
 
     public static void main(String[] args) {
         printHeader();
         calculateTaxesAndSalaryInHand();
-        printFooter();
     }
 
     private static void printHeader() {
-        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------------------------------------------------------");
         System.out.println(" Taxes and Salary");
-        System.out.println("----------------------------------------------------------------------------------------");
-        System.out.printf("| %-10s | %-15s | %18s |%12s |%10s |%n", "DAYS", "STANDARD 10% TAX", "GST 18% INCLUSIVE", "TOTAL TAX", "IN HAND SALARY");
-        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------------------------------------------------------");
+        System.out.printf("| %-10s | %-18s | %-16s | %-19s | %-10s | %-15s |%n", "DAYS", "BILL TO CLIENT", "STANDARD 10% TAX", "GST 18% INCLUSIVE", "TOTAL TAX", "IN HAND SALARY");
+        System.out.println("-----------------------------------------------------------------------------------------------------------");
     }
 
     private static void calculateTaxesAndSalaryInHand() {
@@ -27,20 +27,23 @@ public class GSTInclusiveCalculator {
             double gstAmount = totalBillToClient * GST_PERCENTAGE / (100 + GST_PERCENTAGE);
             int tenPercentFlatTax = totalBillToClient / 10;
             double inHandSalary = totalBillToClient - (gstAmount + tenPercentFlatTax);
-            printResult(days, tenPercentFlatTax, gstAmount, (gstAmount + tenPercentFlatTax), inHandSalary);
+            printResult(days, totalBillToClient, tenPercentFlatTax, gstAmount, (gstAmount + tenPercentFlatTax), inHandSalary);
         }
     }
 
-    private static void printResult(int days, int tenPercentFlatTax, double gstAmount, double totalTax, double inHandSalary) {
-        String formattedGstAmount = DECIMAL_FORMAT.format(gstAmount);
-        String formattedTotalTax = DECIMAL_FORMAT.format(totalTax);
-        String formattedInHandSalary = DECIMAL_FORMAT.format(inHandSalary);
+    private static void printResult(int days, int totalBillToClient, int tenPercentFlatTax, double gstAmount, double totalTax, double inHandSalary) {
+        String formattedTotalBillToClient = formatIndianNumber(totalBillToClient);
+        String formattedTenPercentFlatTax = formatIndianNumber(tenPercentFlatTax);
+        String formattedGstAmount = formatIndianNumber(gstAmount);
+        String formattedTotalTax = formatIndianNumber(totalTax);
+        String formattedInHandSalary = formatIndianNumber(inHandSalary);
 
-        System.out.printf("| %-10d | %-15d | %18s | %-12s | %-10s |%n", days, tenPercentFlatTax, formattedGstAmount, formattedTotalTax, formattedInHandSalary);
-        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.printf("| %-10s | %-18s | %-16s | %-19s | %-10s | %-15s |%n", days, formattedTotalBillToClient, formattedTenPercentFlatTax, formattedGstAmount, formattedTotalTax, formattedInHandSalary);
+        System.out.println("-----------------------------------------------------------------------------------------------------------");
     }
 
-    private static void printFooter() {
-        System.out.println("----------------------------------------------------------------------------------------");
+    private static String formatIndianNumber(double value) {
+        Format format = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        return format.format(value);
     }
 }
